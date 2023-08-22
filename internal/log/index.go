ab39e7950f425ad2f35715f7aa2e427787f94a8c
Wolves <cstingl@protonmail.com>
2023-08-22 16:16:03 -0400

    Add log index with newIndex function

---
+++ internal/log/index.go
@@ -0,0 +1,43 @@
+package log
+
+import (
+	"os"
+
+	"github.com/tysonmote/gommap"
+)
+
+var (
+	offWidth uint64 = 4
+	posWidth uint64 = 8
+	entWidth        = offWidth + posWidth
+)
+
+type index struct {
+	file *os.File
+	mmap gommap.MMap
+	size uint64
+}
+
+func newIndex(f *os.File, c Config) (*index, error) {
+	idx := &index{
+		file: f,
+	}
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	idx.size = uint64(fi.Size())
+	if err = os.Truncate(
+		f.Name(), int64(c.Segment.MaxIndexBytes),
+	); err != nil {
+		return nil, err
+	}
+	if idx.mmap, err = gommap.Map(
+		idx.file.Fd(),
+		gommap.PROT_READ|gommap.PROT_WRITE,
+		gommap.MAP_SHARED,
+	); err != nil {
+		return nil, err
+	}
+	return idx, nil
+}
